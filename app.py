48b9629e6711a2f29866fccd438eae73168063f2
NuKe1 <71056184+JonathanSierra@users.noreply.github.com>
2025-05-04 03:09:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ db = client["formularioCustomers"]
 def createApp():
     app = Flask(__name__)
     CORS(app)
+
+    @app.route("/", methods=["GET"])
+    def home():
+        return jsonify({"mensaje":"API funcionando correctamente"}), 200
     
     @app.route("/guardar", methods=["POST"])
     def guardar():
